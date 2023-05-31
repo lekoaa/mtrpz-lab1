dee344e6711c435b3a8fbd0a61fdfcdd704aba79
Andriyaschenko Vladimir <vovan4ik041@gmail.com>
2023-05-31 14:29:02 +0300

    Changed method of solving in equation.py

--- equation.py
+++ equation.py
@@ -1,10 +1,14 @@
-import sympy
+from math import sqrt
 
-def equation(a, b, c):
-
-    x = sympy.symbols('x')
-
-    equation = sympy.Eq(a * x**2 + b * x + c, 0)
-    solutions = sympy.solve(equation, x)
-    
-    return solutions
+def equation (a, b, c):
+    print(f'Equation is: ({a}) x^2 + ({b}) x + ({c}) = 0')
+    D = b ** 2 - 4 * a * c
+    if D > 0:
+        x1 = (-b + sqrt(D)) / 2 * a
+        x2 = (-b - sqrt(D)) / 2 * a
+        print(f'There are 2 roots:\nx1 = {x1}\nx2 = {x2}')
+    elif D == 0:
+        x = -b / 2 * a
+        print(f'There is one root: x = {x}')
+    else:
+        print('There are no roots in area of real numbers')
